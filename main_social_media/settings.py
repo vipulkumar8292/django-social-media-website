be562e5531dcb4d3142a86b67ab75e60aa075049
Vipul Kumar <53464204+vipulkumar8292@users.noreply.github.com>
2021-08-27 14:22:00 +0530

    Update settings.py

--- main_social_media/settings.py
+++ main_social_media/settings.py
@@ -149,7 +149,6 @@ USE_TZ = True
   
    
     
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #STATIC_URL = '/static/'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -158,6 +157,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
